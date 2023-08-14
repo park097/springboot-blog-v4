14e241f440ccf5123df03f869d842ed7751218cf
park097 <alslalslclq24@naver.com>
2023-08-14 15:12:29 +0900

    로그인

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -1,29 +1,55 @@
 package shop.mtcoding.blogv2.user;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import shop.mtcoding.blogv2._core.util.Script;
+import shop.mtcoding.blogv2.board.BoardService;
 
 @Controller
 public class UserController {
 
-     @Autowired  //di(컴포넌스 스캔이 됐다는 가정하에)
+    @Autowired  //di(컴포넌스 스캔이 됐다는 가정하에)
+
     private UserService userService;
 
+    @Autowired
+    private HttpSession session;
+
+    // C - V
     @GetMapping("/joinForm")
-    public String joinForm(){
+    public String joinForm() {
         return "user/joinForm";
     }
 
-    //M-V-c
+    // M - V - C
     @PostMapping("/join")
-    public String join(UserRequest.JoinDTO joinDTO){
+    public String join(UserRequest.JoinDTO joinDTO) {
+        // 10초짜리 코드
         userService.회원가입(joinDTO);
-
-        return"user/loginForm";  //persist 초기화 ,퍼시스트 공간이 독립적으로 존재
-                                  //여기서 clear
-
+        return "user/loginForm"; //persist 초기화 ,퍼시스트 공간이 독립적으로 존재
+        //여기서 clear
     }
 
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @PostMapping("/login")
+    public @ResponseBody String login(UserRequest.LoginDTO loginDTO) {
+        User sessionUser = userService.로그인(loginDTO);
+        if (sessionUser == null) {
+            return Script.back("로그인 실패");
+        }
+        session.setAttribute("sessionUser", sessionUser);
+        return Script.href("/");
+    }
 }
